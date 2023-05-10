c562f3cd356d067a22db3503740d146ae9d9a9fd
taylorwood95 <115211674+taylorwood95@users.noreply.github.com>
2023-05-10 16:01:33 +0200

    Account controller completed.

--- back_end/src/main/java/com/expenseTracker/ExpenseTracker/components/DataLoader.java
+++ back_end/src/main/java/com/expenseTracker/ExpenseTracker/components/DataLoader.java
@@ -34,34 +34,19 @@ public class DataLoader implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
 
         User user1 = new User("Taylor");
-        userRepository.save(user1);
 
 
 
 
 
         Account account1 = new Account("Everyday Account", 5000, user1);
-//        Expense expense1 = new Expense("Electricity", 120.0, CategoryType.BILLS);
-//        expenseRepository.save(expense1);
+        Account account2 = new Account("Savings Account", 10000, user1);
+
+        Expense expense1 = new Expense("Netflix", 15, CategoryType.SUBSCRIPTIONS, user1);
+
+
+
 //
-//
-//        Expense expense2 = new Expense("Netflix", 12.50, CategoryType.SUBSCRIPTIONS);
-//        expenseRepository.save(expense2);
-//
-//        Expense expense3 = new Expense("Mortgage", 550, CategoryType.MORTGAGE);
-//        expenseRepository.save(expense3);
-//
-//        Expense expense4 = new Expense("Gig", 30, CategoryType.ENTERTAINMENT);
-//        expenseRepository.save(expense4);
-//
-//        Expense expense5 = new Expense("Eggs", 2.50, CategoryType.GROCERIES);
-//        expenseRepository.save(expense2);
-//
-//        Expense expense6 = new Expense("Nandos", 36.20, CategoryType.EATING_OUT);
-//        expenseRepository.save(expense6);
-//
-//        Expense expense7 = new Expense("Bus Fare", 1.50, CategoryType.TRANSPORT);
-//        expenseRepository.save(expense2);
 
     }
 
