1f7c5ca618bd9f89fff0a1f16db3e3cb30637430
taylorwood95 <115211674+taylorwood95@users.noreply.github.com>
2024-06-11 12:34:49 +0100

    added cors config in back-end

--- back_end/src/main/java/com/expenseTracker/ExpenseTracker/ExpenseTrackerApplication.java
+++ back_end/src/main/java/com/expenseTracker/ExpenseTracker/ExpenseTrackerApplication.java
@@ -18,7 +18,11 @@ public class ExpenseTrackerApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/**").allowedOrigins("http://localhost:8080", "http://localhost:3000/")
+						.allowedMethods("GET", "POST", "PUT",
+								"DELETE", "OPTIONS")
+						.allowCredentials(true)
+						.allowedHeaders("*");
 			}
 		};
 	}
